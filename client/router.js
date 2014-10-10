b2da0df4a4c057634c493ac3a32a5ba0cca8ab1d
Michael Garvin <gar@andyet.net>
2014-10-10 16:32:05 -0700

    Add fitocracy page

--- client/router.js
+++ client/router.js
@@ -1,11 +1,16 @@
 var Router = require('ampersand-router');
 var HomePage = require('./pages/home');
+var FitocracyPage = require('./pages/fitocracy');
 
 module.exports = Router.extend({
     routes: {
-        '': 'home'
+        '': 'home',
+        'fitocracy': 'fitocracy'
     },
     home: function () {
         this.trigger('page', new HomePage());
+    },
+    fitocracy: function () {
+        this.trigger('page', new FitocracyPage());
     }
 });

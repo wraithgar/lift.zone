a23f56b15491578f10b35a1e86aff4c0a4a88dea
Michael Garvin <gar@andyet.net>
2014-10-10 19:45:04 -0700

    About page

--- client/router.js
+++ client/router.js
@@ -2,11 +2,13 @@
 var Router = require('ampersand-router');
 var HomePage = require('./pages/home');
 var FitocracyPage = require('./pages/fitocracy');
+var AboutPage = require('./pages/about');
 
 module.exports = Router.extend({
     routes: {
         '': 'home',
-        'fitocracy': 'fitocracy'
+        'fitocracy': 'fitocracy',
+        'about': 'about'
     },
     home: function () {
         app.activities.reset();
@@ -15,5 +17,8 @@ module.exports = Router.extend({
     fitocracy: function () {
         app.activities.reset();
         this.trigger('page', new FitocracyPage());
+    },
+    about: function () {
+        this.trigger('page', new AboutPage());
     }
 });

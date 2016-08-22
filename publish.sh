358eb082dded05a5eaec9143df8ff6e7d84b07c8
Michael Garvin <gar+gh@danger.computer>
2016-08-22 11:30:14 -0700

    update news, consolidate publish scripts

--- publish.sh
+++ publish.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+s3cmd sync ./assets/ s3://assets.lift.zone -v --acl-public --delete-removed --guess-mime-type --config ./.s3cfg
 s3cmd sync ./public/ s3://lift.zone -v --acl-public --delete-removed -m text/html --config ./.s3cfg
--- publish_assets.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-s3cmd sync ./assets/ s3://assets.lift.zone -v --acl-public --delete-removed --guess-mime-type --config ./.s3cfg
